2c2ed03e2abe22fe4b4e5dc5b20ac3b9f69a090d
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-09-28 14:14:59 +0200

    #Das ist eine Änderung als Commit zu Github geht und dann zu pythonanywhere

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -127,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Das ist eine Änderung als Commit zu Github geht und dann zu pythonanywhere
+
