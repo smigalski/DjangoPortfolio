4bd403578d8bf2ca368ded35b2db52c767a5bef8
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-09-28 14:03:18 +0200

    Testen von updating files from github zu pythonanywhere

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ayn959y9$gc1!7!mouxs8+9b%hcua!vvtdi$_ms6_wydpnxnd4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['danielsmigala01.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
