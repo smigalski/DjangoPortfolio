1af491a9e64b3ed3aba74a805c1af77155357cd5
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-10-02 19:07:19 +0200

    Tags erscheinen in fett. Die in der Beschreibung hinzugefügten Abstände werden übernommen, linebreaks. Debug in settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ayn959y9$gc1!7!mouxs8+9b%hcua!vvtdi$_ms6_wydpnxnd4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
